8a44aae7c8b20ae56625e13f7316fe39d8adcbb2
Michael Skogberg <michael.skogberg@futurice.com>
2018-07-08 13:24:12 +0300

    Initial Commit

---
+++ BoatTracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BoatTracker
+//
+//  Created by Michael Skogberg on 08/07/2018.
+//  Copyright © 2018 Michael Skogberg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
