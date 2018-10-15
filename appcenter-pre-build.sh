2de5d1aa7d5c95c016291dd374e9634c66dcc252
Michael Skogberg <michael.skogberg@futurice.com>
2018-10-16 00:54:48 +0300

    Bug fixes and improvements

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -3,8 +3,8 @@
 # Injects credentials to a plist file
 if [ "$APPCENTER_BRANCH" == "master" ];
 then
-	cp $APPCENTER_SOURCE_DIRECTORY/BoatTracker/Credentials-template.plist $APPCENTER_SOURCE_DIRECTORY/BoatTracker/Credentials.plist
-    plutil -replace MapboxAccessToken -string "\$(MapboxAccessToken)" $APPCENTER_SOURCE_DIRECTORY/BoatTracker/Credentials.plist
-    plutil -replace GoogleClientId -string "\$(GoogleClientId)" $APPCENTER_SOURCE_DIRECTORY/BoatTracker/Credentials.plist
+    cp $APPCENTER_SOURCE_DIRECTORY/BoatTracker/Credentials-template.plist $APPCENTER_SOURCE_DIRECTORY/BoatTracker/Credentials.plist
+    plutil -replace MapboxAccessToken -string "$MapboxAccessToken" $APPCENTER_SOURCE_DIRECTORY/BoatTracker/Credentials.plist
+    plutil -replace GoogleClientId -string "$GoogleClientId" $APPCENTER_SOURCE_DIRECTORY/BoatTracker/Credentials.plist
 fi
 
