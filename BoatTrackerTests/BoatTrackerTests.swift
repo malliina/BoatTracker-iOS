88d45e48b61c48c7977cbf12dec4eb2772b6c4f1
Michael Skogberg <michael.skogberg@futurice.com>
2019-02-17 21:48:32 +0200

    Fix tests

--- BoatTrackerTests/BoatTrackerTests.swift
+++ BoatTrackerTests/BoatTrackerTests.swift
@@ -27,43 +27,13 @@ class BoatTrackerTests: XCTestCase {
         XCTAssert(isSuccess)
     }
     
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        let input = "{\"a\": 42, \"b\": {\"sub\": \"hello\"}}"
-        let obj = try JsObject.parse(string: input)
-        let actual = try obj.readInt("a")
-        XCTAssert(actual == 42)
-        let b = try obj.readObject("b")
-        let sub = try b.readString("sub")
-        XCTAssert(sub == "hello")
-        XCTAssertThrowsError(try obj.readString("nonexistent"), "Reading nonexistent key throws") { (error) in
-            if case JsonError.missing(_) = error {
-                
-            } else {
-                XCTAssert(false, "Wrong error type.")
-            }
-        }
-    }
-    
-    func testDictToJson() throws {
-        let dict = ["boatName": "abc" as AnyObject]
-        let data = try JSONSerialization.data(withJSONObject: dict, options: [])
-        if let json = Json.asJson(data) {
-            let str = Json.stringify(json)
-            XCTAssert(str != nil && !(str?.isEmpty ?? false))
-        } else {
-            XCTAssert(false, "Not JSON.")
-        }
-    }
-    
     func testKeychain() throws {
         let kc = Keychain.shared
         XCTAssertNil(try kc.findToken())
-        let initial = AccessToken(token: "abc")
+        let initial = AccessToken("abc")
         try kc.use(token: initial)
         XCTAssertEqual(try kc.readToken(), initial)
-        let updated = AccessToken(token: "def")
+        let updated = AccessToken("def")
         try kc.use(token: updated)
         XCTAssertEqual(try kc.readToken(), updated)
         try kc.delete()
