bf59965cf82012f3483f3ca547e4152b85a46a10
Michael Skogberg <malliina123@gmail.com>
2022-05-09 20:37:38 +0300

    Fastlane release

--- fastlane/Appfile.swift
+++
@@ -1,9 +0,0 @@
-var appIdentifier: String { return "com.malliina.BoatTracker" } // The bundle identifier of your app
-var appleID: String { return "malliina123@gmail.com" } // Your Apple email address
-
-var itcTeam: String? { return "117713845" } // App Store Connect Team ID
-var teamID: String { return "D2T2QC36Z9" } // Apple Developer Portal Team ID
-
-
-// For more information about the Appfile, see:
-//     https://docs.fastlane.tools/advanced/#appfile
