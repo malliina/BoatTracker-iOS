18ffb91b4a1e11cd9a0ea04a1c50bb1dbbcca20e
Michael Skogberg <michael.skogberg@futurice.com>
2023-06-11 19:37:14 +0300

    Chart units

--- BoatTracker/Views/ChartsView.swift
+++ BoatTracker/Views/ChartsView.swift
@@ -41,13 +41,13 @@ struct ChartRepresentable: UIViewRepresentable {
     }
     
     func makeCoordinator() -> Coordinator {
-        Coordinator(lang: lang.tracks)
+        Coordinator(lang: lang.tracks, isBoat: vm.data?.from.sourceType.isBoat ?? true)
     }
     
     func makeUIView(context: Context) -> LineChartView {
         let chart = LineChartView(frame: CGRect(x: 0, y: 0, width: 64, height: 64))
         let coordinator = context.coordinator
-        chart.data = LineChartData(dataSets: [coordinator.speedDataSet, coordinator.depthDataSet])
+        chart.data = LineChartData(dataSets: [coordinator.speedDataSet, coordinator.blueDataSet])
         chart.xAxis.valueFormatter = TimeFormatter(formatting: lang.formats)
         chart.xAxis.labelPosition = .bottom
         return chart
@@ -64,28 +64,42 @@ struct ChartRepresentable: UIViewRepresentable {
     class Coordinator {
         let seaBlue = UIColor(red: 0x00, green: 0x69, blue: 0x94)
         let speedDataSet: LineChartDataSet
-        let depthDataSet: LineChartDataSet
+        let blueDataSet: LineChartDataSet
+        let lang: TrackLang
         
-        init(lang: TrackLang) {
-            speedDataSet = LineChartDataSet(entries: [], label: "\(lang.speed) (kn)")
+        init(lang: TrackLang, isBoat: Bool) {
+            self.lang = lang
+            let unit = isBoat ? "kn" : "km/h"
+            speedDataSet = LineChartDataSet(entries: [], label: "\(lang.speed) (\(unit)")
             speedDataSet.colors = [ .red ]
             speedDataSet.circleRadius = 0
             speedDataSet.circleHoleRadius = 0
-            depthDataSet = LineChartDataSet(entries: [], label: "\(lang.depth) (m)")
-            depthDataSet.colors = [ seaBlue ]
-            depthDataSet.circleRadius = 0
-            depthDataSet.circleHoleRadius = 0
+            blueDataSet = LineChartDataSet(entries: [], label: "\(isBoat ? lang.depth : lang.env.altitude) (m)")
+            blueDataSet.colors = [ seaBlue ]
+            blueDataSet.circleRadius = 0
+            blueDataSet.circleHoleRadius = 0
         }
         
         func updateDataSet(event: CoordsData) {
+            let isBoat = event.from.sourceType.isBoat
             let speedEntries = event.coords.map { c in
-                ChartDataEntry(x: Double(c.time.millis), y: c.speed.knots, data: c.time.dateTime as AnyObject)
+                ChartDataEntry(x: Double(c.time.millis), y: isBoat ? c.speed.knots : c.speed.kph, data: c.speed as AnyObject)
             }
+            let speedUnit = isBoat ? "kn" : "km/h"
+            speedDataSet.label = "\(lang.speed) (\(speedUnit))"
+            speedDataSet.replaceEntries(speedDataSet.entries + speedEntries)
+            
             let depthEntries = event.coords.map { c in
                 ChartDataEntry(x: Double(c.time.millis), y: c.depthMeters.meters, data: c.depthMeters as AnyObject)
             }
-            speedDataSet.replaceEntries(speedDataSet.entries + speedEntries)
-            depthDataSet.replaceEntries(depthDataSet.entries + depthEntries)
+            let altitudeEntries: [ChartDataEntry] = event.coords.compactMap { c in
+                guard let altitude = c.altitude else { return nil }
+                return ChartDataEntry(x: Double(c.time.millis), y: altitude.meters, data: c.altitude as AnyObject)
+            }
+            let blueMetric = isBoat ? lang.depth : lang.env.altitude
+            blueDataSet.label = "\(blueMetric) (m)"
+            let blueEntries = blueDataSet.entries + (isBoat ? depthEntries : altitudeEntries)
+            blueDataSet.replaceEntries(blueEntries)
         }
     }
     
