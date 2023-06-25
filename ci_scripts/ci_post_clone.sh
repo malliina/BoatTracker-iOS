38c729ad8f46602ec7fc1cda9dd21445bd20fb01
Michael Skogberg <michael.skogberg@futurice.com>
2023-06-25 20:10:21 +0300

    Fix CI path

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -2,7 +2,7 @@
 set -e
 mkdir ~/.apple/
 echo "$CREDENTIALS_PLIST" > ~/.apple/Credentials.plist.b64
-base64 -d -i ~/.apple/Credentials.plist.b64 > BoatTracker/Credentials.plist
+base64 -d -i ~/.apple/Credentials.plist.b64 > $CI_PRIMARY_REPOSITORY_PATH/BoatTracker/Credentials.plist
 echo "$NETRC" > ~/.apple/netrc.b64
 base64 -d -i ~/.apple/netrc.b64 > ~/.netrc
 chmod 600 ~/.netrc
